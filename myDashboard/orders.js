228c5bcff0b4f7a6ceec8e4a691896013a50375a
jmjgenero@gmail.com <jmjgenero@gmail.com>
2023-03-02 00:46:27 +0300

    Add new project file

---
+++ myDashboard/orders.js
@@ -0,0 +1,23 @@
+const Orders = [
+    {
+        productName : 'Camera canon',
+        productNumber :'8565',
+        paymentStatus : 'Paid',
+        shipping:'Delivered'
+
+    },
+    {
+        productName : 'Camera canon',
+        productNumber :'3556',
+        paymentStatus : 'Due',
+        shipping: 'Pending'
+
+    },
+    {
+        productName : 'Camera canon',
+        productNumber :'345',
+        paymentStatus : 'Due',
+        shipping: 'Declined'
+
+    }
+]
